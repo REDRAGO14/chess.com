b29fb53c1cca607c8d9cce9befa0133679ada5cc
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-14 00:40:07 -0700

    handled DTO LEVEL field validation exceptions IN GlobalExceptionHandler

--- src/main/java/com/game/chess/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/game/chess/Exception/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import com.game.chess.DTO.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +19,12 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "USERNAME_EXIST", ex.getMessage(), request.getRequestURI());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
-
+    //here i have handled dto level validation
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest req){
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "VALIDATION_FAILD", ex.getMessage(), req.getRequestURI(), fieldErrors);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
