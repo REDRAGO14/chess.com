1df8be8d08574d7db38890498e250f9918e13daa
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-22 13:02:24 -0700

    implemented CustomAuthenticationManager  to use custom daoAuthenticationProvider

--- src/main/java/com/game/chess/Security/SecurityConfig.java
+++ src/main/java/com/game/chess/Security/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.game.chess.Security;
 
+import com.game.chess.Repository.UserRepository;
+import com.game.chess.Service.CustomUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http){
         http
@@ -22,5 +34,15 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomUserDetailService(userRepository);
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService());
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
