547f6e5e610d3b62969d3cab3fc5a23e6cf9f383
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-22 12:53:08 -0700

    implemented loadByUserName methode in UserRepository to fetch user from db

--- src/main/java/com/game/chess/Repository/UserRepository.java
+++ src/main/java/com/game/chess/Repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.game.chess.Repository;
 
 import com.game.chess.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUserName(String userName);
+
+    UserDetails loadByUserName(String username);
 }
