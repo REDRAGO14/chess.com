194832c1344550de767a1a1bb7699c532a9236d9
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-23 04:25:15 -0700

    change the findByUserName method return type to Optional

--- src/main/java/com/game/chess/Repository/UserRepository.java
+++ src/main/java/com/game/chess/Repository/UserRepository.java
@@ -2,14 +2,12 @@ package com.game.chess.Repository;
 
 import com.game.chess.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByUserName(String userName);
-
-    UserDetails loadByUserName(String username);
+    Optional<User> findByUserName(String username);
 }
