47c103030910d22c9d539aa83519772d8303be66
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-24 11:32:43 -0700

    injected JWTUtil at AuthController by utilizing constructor injection

--- src/main/java/com/game/chess/Controller/AuthController.java
+++ src/main/java/com/game/chess/Controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.game.chess.Controller;
 
 import com.game.chess.Model.AuthRequest;
+import com.game.chess.Util.JWTUtil;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
 
-    public AuthController(AuthenticationManager authenticationManager) {
+    public AuthController(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/login")
