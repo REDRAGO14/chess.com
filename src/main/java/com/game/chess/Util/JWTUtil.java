c3540740555f032a38cf3c690ee55049d39a5cdd
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-24 11:30:46 -0700

    added 32-bit SECRET string

--- src/main/java/com/game/chess/Util/JWTUtil.java
+++ src/main/java/com/game/chess/Util/JWTUtil.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 @Component
 public class JWTUtil {
-    private final String SECRET = "j";
+    private final String SECRET = "eX9+Z1P2qLmN4vRw8kY3tA6sD0fG1hJ5kL7mNpQrStUvWxYz0aBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUvWw==";
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
     private final int expireAT = 1000*60*60;
 
